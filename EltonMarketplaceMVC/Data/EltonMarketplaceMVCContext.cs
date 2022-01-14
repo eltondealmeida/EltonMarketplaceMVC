48849baa6b66c8b6ed155bad2f45d58baf89b607
Elton de Almeida <elton_de_almeida@hotmail.com>
2022-01-13 23:16:20 -0300

    CRUD Scaffolding

---
+++ EltonMarketplaceMVC/Data/EltonMarketplaceMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EltonMarketplaceMVC.Models;
+
+namespace EltonMarketplaceMVC.Data
+{
+    public class EltonMarketplaceMVCContext : DbContext
+    {
+        public EltonMarketplaceMVCContext (DbContextOptions<EltonMarketplaceMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EltonMarketplaceMVC.Models.Department> Department { get; set; }
+    }
+}
--- EltonMarketplaceMVC/Startup.cs
+++ EltonMarketplaceMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EltonMarketplaceMVC.Data;
 
 namespace EltonMarketplaceMVC
 {
@@ -33,6 +35,9 @@ namespace EltonMarketplaceMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<EltonMarketplaceMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("EltonMarketplaceMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
