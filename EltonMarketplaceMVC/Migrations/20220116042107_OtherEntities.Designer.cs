b1f0801b95c2c7ec21d53fabf12e3e79f96a7dde
Elton de Almeida <elton_de_almeida@hotmail.com>
2022-01-16 01:23:25 -0300

    Other entities and second migration

--- EltonMarketplaceMVC/Migrations/20220116042107_OtherEntities.Designer.cs
+++ EltonMarketplaceMVC/Migrations/20220116042107_OtherEntities.Designer.cs
@@ -4,14 +4,16 @@ using EltonMarketplaceMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EltonMarketplaceMVC.Migrations
 {
     [DbContext(typeof(EltonMarketplaceMVCContext))]
-    partial class EltonMarketplaceMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20220116042107_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
