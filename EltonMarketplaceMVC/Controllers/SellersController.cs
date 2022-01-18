d518f76d0496deac117d9d5aa395a9a4b9c97575
Elton de Almeida <elton_de_almeida@hotmail.com>
2022-01-18 00:34:10 -0300

    Simple Create Form

--- EltonMarketplaceMVC/Controllers/SellersController.cs
+++ EltonMarketplaceMVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EltonMarketplaceMVC.Services;
+using EltonMarketplaceMVC.Models;
 
 namespace EltonMarketplaceMVC.Controllers
 {
@@ -19,6 +20,18 @@ namespace EltonMarketplaceMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
